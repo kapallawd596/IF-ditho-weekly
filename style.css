572f07421d91f8c6eacd3e32e02a662f9b5e95eb
ditho dewangga <dithodewangga551@gmail.com>
2026-04-29 11:13:51 +0700

    penambahan css pada semua halaman

---
+++ style.css
@@ -0,0 +1,156 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f6f8;
+    margin: 20px;
+}
+
+h1 {
+    text-align: center;
+    color: #333;
+}
+
+h2 {
+    text-align: center;
+    margin-top: 20px;
+    color: #333;
+}
+
+/* Garis */
+hr {
+    margin: 30px 0;
+    border: none;
+    height: 1px;
+    background-color: #1b1616;
+}
+
+/* Link */
+a {
+    text-decoration: none;
+}
+
+/* NAVBAR */
+
+
+table:first-of-type td {
+    background-color: #007bff;
+    transition: 0.3s;
+}
+
+table:first-of-type td:hover {
+    background-color: #0056b3;
+}
+
+table:first-of-type a {
+    color: white;
+    font-weight: bold;
+}
+
+table:first-of-type a:hover {
+    text-decoration: underline;
+}
+
+
+button {
+    display: block;
+    margin: 15px auto;
+    background-color: #28a745;
+    color: white;
+    border: 1px solid #1e7e34;
+    padding: 10px 15px;
+    cursor: pointer;
+    border-radius: 5px;
+}
+
+button:hover {
+    background-color: #218838;
+}
+
+table {
+    border-collapse: collapse;
+    margin: 20px auto;
+    width: 90%;
+    background-color: white;
+    border: 2px solid #3d3434;
+}
+
+th {
+    background-color: #007bff;
+    color: white;
+}
+
+td, th {
+    text-align: center;
+    padding: 10px;
+    border: 1px solid #000;
+}
+
+tr:hover {
+    background-color: #f1f9ff;
+}
+
+
+img {
+    display: block;
+    margin: 20px auto;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+}
+
+
+/* HALAMAN INDEX */
+
+p {
+    width: 80%;
+    margin: 20px auto;
+    line-height: 1.6;
+    background-color: white;
+    padding: 15px;
+    border-radius: 5px;
+    border: 1px solid #ccc; /* dibuat lebih soft */
+}
+
+ul {
+    width: 80%;
+    margin: 20px auto;
+}
+
+ul li {
+    margin: 5px 0;
+}
+
+
+/* HALAMAN PROFILE */
+
+.profile-container {
+    width: 80%;
+    margin: 30px auto;
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    background-color: white;
+    padding: 15px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+}
+
+.profile-container img {
+    width: 150px;
+    border-radius: 10px;
+}
+
+.profile-text {
+    flex: 1;
+}
+
+/* HALAMAN FORM */
+
+.form-page form table {
+    width: 50%;
+}
+
+.form-page input {
+    width: 100%;
+    padding: 6px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
