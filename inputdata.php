af3b0c59776bd86c1679680b495a168bcf8846d1
ditho dewangga <dithodewangga551@gmail.com>
2026-05-29 20:32:26 +0700

    update project php laravel laragon

--- inputdata.php
+++ inputdata.php
@@ -1,3 +1,41 @@
+<?php
+
+$conn = mysqli_connect("localhost","root","","dithoweekly-a");
+
+if(isset($_POST['submit'])){
+
+    $nama     = $_POST['nama'];
+    $nim      = $_POST['nim'];
+    $jurusan  = $_POST['jurusan'];
+    $email    = $_POST['email'];
+    $no_hp    = $_POST['no_hp'];
+
+    $foto = $_FILES['foto']['name'];
+    $tmp  = $_FILES['foto']['tmp_name'];
+
+    move_uploaded_file($tmp, "assets/images/" . $foto);
+
+    mysqli_query($conn,
+    "INSERT INTO mahasiswa
+    (nama,nim,jurusan,email,no_hp,foto)
+
+    VALUES
+
+    (
+        '$nama',
+        '$nim',
+        '$jurusan',
+        '$email',
+        '$no_hp',
+        '$foto'
+    )");
+
+    header("Location: mahasiswa.php");
+    exit;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,38 +49,62 @@
 
 <h2>Tambah Data Mahasiswa</h2>
 
-<form action="mahasiswa.php" method="post">
+<form action="" method="post" enctype="multipart/form-data">
+
     <table border="1" cellpadding="5">
+
         <tr>
             <td><label for="nama">Nama</label></td>
-            <td><input type="text" name="nama" id="nama"></td>
-        </tr>
-        <tr>
-            <td><label for="nim">NIM</label></td>
-            <td><input type="number" name="nim" id="nim"></td>
-        </tr>
-        <tr>
-            <td><label for="foto">Foto</label></td>
-            <td><input type="file" name="foto" id="foto"></td>
-        </tr>
-        <tr>
-            <td><label for="uts">UTS</label></td>
-            <td><input type="number" name="uts" id="uts"></td>
-        </tr>
-        <tr>
-            <td><label for="uas">UAS</label></td>
-            <td><input type="number" name="uas" id="uas"></td>
-        </tr>
-        <tr>
-            <td><label for="tugas">TUGAS</label></td>
-            <td><input type="number" name="tugas" id="tugas"></td>
-        </tr>
-        <tr>
-            <td colspan="2" align="center">
-                <button type="submit">Tambah data</button>
+            <td>
+                <input type="text" name="nama" id="nama" required>
             </td>
         </tr>
+
+        <tr>
+            <td><label for="nim">NIM</label></td>
+            <td>
+                <input type="number" name="nim" id="nim" required>
+            </td>
+        </tr>
+
+        <tr>
+            <td><label for="jurusan">Jurusan</label></td>
+            <td>
+                <input type="text" name="jurusan" id="jurusan">
+            </td>
+        </tr>
+
+        <tr>
+            <td><label for="email">Email</label></td>
+            <td>
+                <input type="email" name="email" id="email">
+            </td>
+        </tr>
+
+        <tr>
+            <td><label for="no_hp">No HP</label></td>
+            <td>
+                <input type="text" name="no_hp" id="no_hp">
+            </td>
+        </tr>
+
+        <tr>
+            <td><label for="foto">Foto</label></td>
+            <td>
+                <input type="file" name="foto" id="foto">
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="2" align="center">
+                <button type="submit" name="submit">
+                    Tambah Data
+                </button>
+            </td>
+        </tr>
+
     </table>
+
 </form>
 
 </body>
--- mahasiswa.php
+++ mahasiswa.php
@@ -1,14 +1,26 @@
+<?php
+
+$conn = mysqli_connect("localhost","root","","dithoweekly-a");
+
+$data = mysqli_query($conn, "SELECT * FROM mahasiswa");
+
+$no = 1;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>mahasiswa</title>
+    <title>Mahasiswa</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
 <h1>WEB INFORMATIKA</h1>
+
 <hr>
 
 <table border="1" cellspacing="0" cellpadding="10">
@@ -20,89 +32,86 @@
     </tr>
 </table>
 
-<h2>data mahasiswa</h2>
+<h2>Data Mahasiswa</h2>
 
 <a href="inputdata.php">
-    <button>tambah data</button>
+    <button>Tambah Data</button>
 </a>
 
 <br><br>
 
 <table border="1" cellspacing="5" cellpadding="10">
+
     <tr>
-        <th >No</th>
+        <th>No</th>
         <th>Nama</th>
         <th>NIM</th>
-        <th>jurusan</th>
-        <th>email</th>
-        <th >no.hp</th>
-        <th>foto</th>
-        <th>aksi</th>
+        <th>Jurusan</th>
+        <th>Email</th>
+        <th>No HP</th>
+        <th>Foto</th>
+        <th>Aksi</th>
     </tr>
+
+    <?php while($row = mysqli_fetch_assoc($data)) : ?>
+
     <tr>
-        <th>UTS</th>
-        <th>UAS</th>
-        <th>TUGAS</th>
+
+        <td align="center">
+            <?= $no++; ?>
+        </td>
+
+        <td>
+            <?= $row['nama']; ?>
+        </td>
+
+        <td align="center">
+            <?= $row['nim']; ?>
+        </td>
+
+        <td align="center">
+            <?= $row['jurusan']; ?>
+        </td>
+
+        <td align="center">
+            <?= $row['email']; ?>
+        </td>
+
+        <td align="center">
+            <?= $row['no_hp']; ?>
+        </td>
+
+        <td align="center">
+
+            <img 
+                src="assets/images/<?= $row['foto']; ?>" 
+                alt="foto mahasiswa"
+                width="100px"
+            >
+
+        </td>
+
+        <td align="center">
+
+            <a href="editdata.php?id=<?= $row['id']; ?>">
+                <button>Edit</button>
+            </a>
+
+            |
+
+            <a 
+                href="hapusdata.php?id=<?= $row['id']; ?>"
+                onclick="return confirm('Yakin ingin menghapus data?')"
+            >
+                <button>Hapus</button>
+            </a>
+
+        </td>
+
     </tr>
-    <tr>
-        <td align="center">1</td>
-        <td>cantona</td>
-        <td align="center">13182420033</td>
-        <td align="center">informatika</td>
-        <td align="center">cantona@example.com</td>
-        <td align="center">08123456789</td>
-        <td align="center"><img src="assets/images/cantona.jpg" alt="foto cantona" width="100px"></td>
-        <td> <a href="editdata.php">
-            <button>edit</button> | <a href="hapusdata.php"><button>hapus</button></a>  
-    </tr>
-</table>
 
-<hr>
+    <?php endwhile; ?>
 
-<table border="1" cellspacing="5">
-<tr>
-    <td>1,1</td>
-        <td align="center">90</td>
-        <td align="center">88</td>
-    </tr>
-</table>
-
-<hr>
-
-<table border="1" cellspacing="5">
-<tr>
-    <td>1,1</td>
-    <td>1,2</td>
-    <td>1,3</td>
-    <td>1,4</td>
-</tr>
-</table>
-
-<table border="1" cellspacing="5">
-<tr>
-    <td>1,1</td>
-    <td>1,2</td>
-    <td>1,3</td>
-    <td>1,4</td>
-</tr>
-</table>
-
-<table border="1" cellspacing="5">
-<tr>
-    <td>1,1</td>
-    <td>1,2</td>
-    <td>1,3</td>
-    <td>1,4</td>
-</tr>
-</table>
-
-<table border="1" cellspacing="5">
-<tr>
-    <td>1,1</td>
-    <td>1,2</td>
-    <td>1,3</td>
-    <td>1,4</td>
-</tr>
 </table>
 
 </body>
